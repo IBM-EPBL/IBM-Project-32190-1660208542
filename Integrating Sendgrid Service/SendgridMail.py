f674346da029f03ea75ac956b454c9e52775f4a8
Kaushik Selvaraju <83058329+Kaushikselvaraju@users.noreply.github.com>
2022-11-18 08:59:23 +0530

    Update

--- Integrating Sendgrid Service/SendgridMail.py
+++ Integrating Sendgrid Service/SendgridMail.py
@@ -2,7 +2,7 @@ import sendgrid
 from sendgrid.helpers.mail import *
 
 def mailtest_request(to_email):
-    sg = sendgrid.SendGridAPIClient(api_key= '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' )
+    # sg = sendgrid.SendGridAPIClient(api_key= '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' )
     from_email = Email("kaushik.selvaraju@gmail.com")
     subject = "Budget Alert !"
     content = Content("text/plain", "Your Expense is out of Budget !")
